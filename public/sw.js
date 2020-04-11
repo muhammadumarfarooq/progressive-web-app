cef3ca3788d89378594b2ff23fd48b61e76ca8c1
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-04-11 17:08:12 +0500

    sw activate

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,8 @@
 self.addEventListener("install", function (event) {
   console.log("installing sw", event);
 });
+
+self.addEventListener("activate", function (event) {
+  console.log("activate sw", event);
+  return self.clients.claim();
+});
